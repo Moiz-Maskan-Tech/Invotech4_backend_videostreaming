f3de668ec4141919af735d739ecdf838f6a9b1c9
Moiz-Maskan-Tech <moiz.rehman@maskantech.com>
2024-12-27 11:23:29 -0800

    update auth.middleware.js and user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,24 +1,31 @@
 import { Router } from "express";
-import { registerUser ,loginUser, logoutUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
 const router = Router();
 import { upload } from "../middlewares/multer.middleware.js";
-import {verifyJWT} from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 // localhost:9090/api/v1/users/registe
-router.route("/register").post( upload.fields([
+router.route("/register").post(
+  upload.fields([
     {
-        name : "avatar",
-        maxCount : 1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-        name : "coverImage",
-        maxCount : 1
-    }
-]) ,registerUser)
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 router.route("/login").post(loginUser);
 
 // secure route
-router.route("/logout").post(verifyJWT , loginUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
